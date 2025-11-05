cba7f4ca7ab974c3809c74c9f493884ac5be1c0c
gzmazek <96499590+gzmazek@users.noreply.github.com>
2025-11-05 10:35:34 +0100

    Deleted useless files and minor changes.

--- graph_generation/__init__.py
+++ graph_generation/__init__.py
@@ -1,5 +1,5 @@
 """Graph generation module for creating LangGraph variants"""
 
-from .langgraph_generator import generate_langgraph_variants
+from .graph_generation import generate_graph_batch
 
-__all__ = ['generate_langgraph_variants']
+__all__ = ['generate_graph_batch']
