1b5296d18ae87aabf877fa035a766153b53e4b7a
vitolev <vito.levstik@gmail.com>
2025-12-29 20:01:47 +0100

    Fix

--- gnn_models/evaluating_models.py
+++ gnn_models/evaluating_models.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
 from data_management.graph_storage import GraphSet
 from gnn_models.model_manager import initialize_gnn_model, predict_batch_performance
 from pathlib import Path
